947b855ad58187e320687564ef895f6aec1a4db0
ekrecaizeYd <119048507+ekrecaizeYd@users.noreply.github.com>
2023-03-17 04:07:45 +0300

    Update hw1.js

--- hw1/hw1.js
+++ hw1/hw1.js
@@ -1,28 +1,3 @@
-function copy_default(btn) {
-  var text_id = btn.name.substr(13, 6)
-  document.getElementById(text_id).value = "Copy"
-  document.getElementById(text_id).style.width = "35px"
-}
-function copy(btn){
-  var text_id = btn.name.substr(0, 12)
-  var text = document.getElementById(text_id)
-  text.select()
-  navigator.clipboard.writeText(text.value)
-}
-function copy_name(btn){
-  var text_id = btn.name.substr(0, 12)
-  var text = document.getElementById(text_id)
-  text.select()
-  navigator.clipboard.writeText(text.name)
-}
-function copied(btn) {
-  var text_id = btn.name.substr(13, 6)
-  document.getElementById(text_id).value = "Copied"
-  document.getElementById(text_id).style.width = "45px"
-}
-function copy_directly(btn) {
-  navigator.clipboard.writeText(btn.value)
-}
 function textbox_underline(box) {
   document.getElementById(box.id).style.borderColor = "rgb( 52,203,203)"
 }
